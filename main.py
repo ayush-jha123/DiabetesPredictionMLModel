c6732af7cbe7db3800dcf7f5ef6289b8529fa6c3
ayush-jha123 <139354914+ayush-jha123@users.noreply.github.com>
2024-05-15 15:14:59 +0530

    Update main.py

--- main.py
+++ main.py
@@ -37,7 +37,7 @@ class model_input(BaseModel):
     
 
 # loading the saved model
-diabetes_model = pickle.load(open('diabetes_model.sav','rb'))
+diabetes_model = pickle.load(open('trained_model.sav','rb'))
 
 
 @app.post('/diabetes_prediction')
